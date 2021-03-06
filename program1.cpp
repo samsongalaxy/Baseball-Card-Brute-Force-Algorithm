9127f7700122893963f867454a419237bae0fd24
samsongalaxy <samsongalaxy@gmail.com>
2021-03-05 21:29:15 -0500

    commit

--- program1.cpp
+++ program1.cpp
@@ -87,8 +87,8 @@ int main(int argc, char *argv[]){
         for(int k = 0; k < n; k++){
           cout << "Current card price: " << i[k].price << "\n";
           cout << "Current card name: " << i[k].name << "\n";
-          string s1 = m[j].get_name();
-          string s2 = i[k].get_name();
+          string s1 = m[j].get_name().substr(0,5);
+          string s2 = i[k].get_name().substr(0,5);
           if(s1 == s2) profit += i[k].price - m[j].price;
           cout << "Current profit: " << profit << "\n";
         }
